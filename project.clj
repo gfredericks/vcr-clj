e43d9d93b75dc972f4e1d1bff7e4f7252c31012d
Gary Fredericks <fredericksgary@gmail.com>
2012-08-15 15:14:50 -0400

    maven group id (how was that not there??)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vcr-clj "0.0.2"
+(defproject com.gfredericks/vcr-clj "0.0.2"
   :description "HTTP recording/playback for Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [fs "1.0.0"]
