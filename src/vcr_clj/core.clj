6b6c8fc823652f36fe4b7c869366f17ed4f5027f
Gary Fredericks <fredericksgary@gmail.com>
2013-01-04 14:26:13 -0600

    Add a couple dynamic vars for better configurability

--- src/vcr_clj/core.clj
+++ src/vcr_clj/core.clj
@@ -38,37 +38,61 @@
 (def req-keys
   [:uri :server-name :server-port :query-string :request-method])
 
+;; These vars are the primary method for customizing the behavior of
+;; vcr-clj.
+(def ^:dynamic record?
+  "Predicate which, given a ring request, determines if it should
+  be recorded or passed through."
+  (constantly true))
+(def ^:dynamic req-key
+  "Given a ring request, returns a key that it should be grouped
+  under. Requests are allowed to come out of order as long as
+  they are in-order with respect to other requests with the same
+  key."
+  #(select-keys % req-keys))
+
+(defn handle-req-by-pred
+  [orig-handler modified-handler request]
+  ((if (record? request) modified-handler orig-handler)
+   request))
+
 (defn- fake-request
   "Given a cassette, returns a replacement (stateful) request function."
-  [cassette]
-  (let [remaining (ref cassette)]
-    (fn [req]
-      (dosync
-       (let [req-key (select-keys req req-keys)
-             resp (first (@remaining req-key))]
-         (when-not resp
-           (throw (Exception. (str "No response in vcr-clj cassette for request: " (pr-str req)))))
-         (alter remaining update req-key rest)
-         resp)))))
+  [orig-handler cassette]
+  (let [remaining (ref cassette)
+        modified-handler
+        (fn [req]
+          (dosync
+           (let [key (req-key req)
+                 resp (first (@remaining key))]
+             (when-not resp
+               (throw (Exception. (str "No response in vcr-clj cassette for request: " (pr-str req)))))
+             (alter remaining update key rest)
+             resp)))]
+    (partial handle-req-by-pred orig-handler modified-handler)))
 
 (defn- record
   [func]
   (let [orig-request clj-http.core/request
-        responses (atom {})]
-    (with-redefs [clj-http.core/request (fn [req] (let [resp (orig-request req)
-                                                        req-key (select-keys req req-keys)]
-                                                    (swap! responses
-                                                           update
-                                                           req-key
-                                                           (fn [x] (conj (or x []) resp)))
-                                                    resp))]
+        responses (atom {})
+        modified-handler
+        (fn [req]
+          (let [resp (orig-request req)
+                key (req-key req)]
+            (swap! responses
+                   update
+                   key
+                   (fn [x] (conj (or x []) resp)))
+            resp))]
+    (with-redefs [clj-http.core/request (partial handle-req-by-pred orig-request modified-handler)]
       (func))
     @responses))
 
 (defn- run-with-existing-cassette
   [cassette func]
-  (with-redefs [clj-http.core/request (fake-request cassette)]
-    (func)))
+  (let [orig clj-http.core/request]
+    (with-redefs [clj-http.core/request (fake-request orig cassette)]
+      (func))))
 
 (defn- cassette-file
   [cassette-name]
