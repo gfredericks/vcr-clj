37611738ae7aaadc8f41bbefb4ec7bd1f1245886
Gary Fredericks <fredericksgary@gmail.com>
2013-07-15 16:50:05 -0500

    Upgrade clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.gfredericks/vcr-clj "0.2.2"
   :description "HTTP recording/playback for Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.codec "0.1.0"]
                  [fs "1.3.3"]]
   :profiles {:dev {:dependencies
