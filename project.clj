937f21f4b1acf323da28c8a1fce1478246cd12aa
Gary Fredericks <fredericksgary@gmail.com>
2013-07-15 16:12:04 -0500

    Upgrade fs.
    The tests still pass so this is probably fine.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "HTTP recording/playback for Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [fs "1.0.0"]]
+                 [fs "1.3.3"]]
   :profiles {:dev {:dependencies
                    [[ring/ring-jetty-adapter "1.1.2"]
                     [clj-http "0.5.3"]]}})
