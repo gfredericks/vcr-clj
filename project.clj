f0a051878b217756a50bb43f0c8a219170f6ce66
Gary Fredericks <fredericksgary@gmail.com>
2015-04-14 08:27:00 -0500

    Version 0.4.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/vcr-clj "0.4.3-SNAPSHOT"
+(defproject com.gfredericks/vcr-clj "0.4.3"
   :description "HTTP recording/playback for Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.codec "0.1.0"]
