1508f8d44fdf052528b26c0e6c3cbe88be381ca5
Gary Fredericks <fredericksgary@gmail.com>
2017-03-01 06:54:52 -0600

    Add new versions of everything

--- project.clj
+++ project.clj
@@ -1,22 +1,29 @@
 (defproject com.gfredericks/vcr-clj "0.4.10-SNAPSHOT"
   :description "HTTP recording/playback for Clojure"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.codec "0.1.0"]
+  :dependencies [[org.clojure/data.codec "0.1.0"]
                  [fs "1.3.3"]]
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev [:test-deps-without-clj-http :clj-http-101]
+  :profiles {:dev [:test-deps-without-clj-http :clj-http-101
+                   :clojure-17]
 
              :test-deps-without-clj-http
-             {:dependencies [[bond "0.2.5"]
-                             [ring/ring-jetty-adapter "1.1.2"]]}
+             [:clojure-17
+              {:dependencies [[bond "0.2.5"]
+                              [ring/ring-jetty-adapter "1.1.2"]]}]
 
+             :clj-http-230 {:dependencies [[clj-http "2.3.0"]]}
+             :clj-http-112 {:dependencies [[clj-http "1.1.2"]]}
              :clj-http-101 {:dependencies [[clj-http "1.0.1"]]}
              :clj-http-091 {:dependencies [[clj-http "0.9.1"]]}
              :clj-http-077 {:dependencies [[clj-http "0.7.7"]]}
-             :clj-http-053 {:dependencies [[clj-http "0.5.3"]]}}
-  :aliases {"test-all"
+             :clj-http-053 {:dependencies [[clj-http "0.5.3"]]}
+             :clojure-17   {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :clojure-18   {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"test-all-clj-https"
             ^{:doc "Runs tests on all listed versions of clj-http."}
             ["do"
+             "clean," "with-profile" "-dev,+test-deps-without-clj-http,+clj-http-230" "test,"
+             "clean," "with-profile" "-dev,+test-deps-without-clj-http,+clj-http-112" "test,"
              "clean," "with-profile" "-dev,+test-deps-without-clj-http,+clj-http-101" "test,"
              "clean," "with-profile" "-dev,+test-deps-without-clj-http,+clj-http-091" "test,"
              "clean," "with-profile" "-dev,+test-deps-without-clj-http,+clj-http-077" "test,"
@@ -24,4 +31,6 @@
              ;; "clean," "with-profile" "-dev,+test-deps-without-clj-http,+clj-http-053" "test"
              ]
             "ci"
-            ["test-all"]})
+            ["do"
+             "test-all-clj-https,"
+             "with-profile" "-clojure-17,+clojure-18" "test-all-clj-https"]})
