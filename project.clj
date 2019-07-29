0886180ef4a92d43e649069feae81334892f6e06
Gary Fredericks <fredericksgary@gmail.com>
2019-07-29 18:32:01 -0500

    Version 0.4.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/vcr-clj "0.4.19-SNAPSHOT"
+(defproject com.gfredericks/vcr-clj "0.4.19"
   :description "HTTP recording/playback for Clojure"
   :dependencies [[org.clojure/data.codec "0.1.0"]
                  [me.raynes/fs "1.4.6"]
